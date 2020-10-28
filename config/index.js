bf8d55c344255f2b7523c359f3253e4f6e242563
Berk <berk@Berk-MacBook-Pro.local>
2020-10-29 01:35:25 +0300

    debugging

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/altevren-review-generator/',
     proxyTable: {},
 
     // Various Dev Server settings
